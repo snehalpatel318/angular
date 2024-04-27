2726d2d34502c31bd5d463a3110fd4a42927933e
snehal patel <snehal.patel@radixweb.com>
2024-04-27 15:49:45 +0530

    parent to child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,24 @@ export class AppComponent {
   obj = {name:'Snehal',age:20} // object 
   arr = ['SnehalPAtel','Shardul','Heer'] // array
 
+  users = [
+    {
+      name:'Snehal',
+      age:25,
+      address:'Himatnagar'
+    },
+    {
+      name:'Shardul',
+      age:28,
+      address:'Himatnagar'
+    },
+    {
+      name:'Heer',
+      age:1,
+      address:'Himatnagar'
+    },
+  ] 
+
   getName(){   // function use
     return this.name;
   }
