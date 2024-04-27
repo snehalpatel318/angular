751c0ed3702bd01a04848dbe436525b55aa663b7
snehal patel <snehal.patel@radixweb.com>
2024-04-27 13:46:54 +0530

    if_else_switch_condition_for_loop_example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,28 @@ export class AppComponent {
   name = "Snehal Patel"; // variable
   obj = {name:'Snehal',age:20} // object 
   arr = ['SnehalPAtel','Shardul','Heer'] // array
+  show = 'false';
+  showif = 'yes'
+  showColor = 'red'
+  color="red"
+  data = ['snehal','shardul','heer']
+
+  multidata = [
+    {
+      name:'Snehal',
+      age:25,
+      email:"xyz@gmail.com"
+    },
+    {
+      name:'Shardul',
+      age:28,
+      email:"xyz@gmail.com"
+    },
+    {
+      name:'Heer',
+      age:1
+    }
+  ]
 
   getName(){   // function use
     return this.name;
