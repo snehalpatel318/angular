c02a8ac89ee2243d2a00a1e9454143289796bed2
snehal patel <snehal.patel@radixweb.com>
2024-05-24 22:20:12 +0530

    localization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,13 @@ export class AppComponent {
   name = "Snehal Patel"; // variable
   obj = {name:'Snehal',age:20} // object 
   arr = ['SnehalPAtel','Shardul','Heer'] // array
+
+  constructor(private translatService:TranslateService){
+    const userLang = navigator.language || 'en';
+    const languageCode =userLang.split('-')[0];
+    this.translatService.setDefaultLang(languageCode);
+    this.translatService.use(languageCode);
+  }
 
   getName(){   // function use
     return this.name;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
 
+export function HttpLoaderFactory(http:HttpClient){
+  return new TranslateHttpLoader(http,'./assets/i18n/','.json');
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +18,15 @@ import { UserListComponent } from './user-list/user-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader:{
+        provide:TranslateLoader,
+        useFactory:HttpLoaderFactory,
+        deps:[HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
