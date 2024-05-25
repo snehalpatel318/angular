6515ebfba96eae02fbe28be51e7072c665208ab0
snehal patel <snehal.patel@radixweb.com>
2024-05-25 15:05:42 +0530

    http data get and post api

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
 
+  public getJsonValue:any;
+  public postJsonValue:any;
+  constructor(private http:HttpClient){
+
+  }
+  ngOnInit(): void {
+    this.getMethod();
+    this.postMethod();
+  }
+
+  getMethod(){
+    this.http.get('https://jsonplaceholder.typicode.com/posts/1').subscribe((data)=>{
+      console.log(data)
+      this.getJsonValue = data;
+    });
+  }
+
+  postMethod(){
+    const header = new HttpHeaders({
+      contentType:'application/json'
+    })
+    let body = {
+      title: 'test',
+    body: 'test',
+    userId: 1,
+    }
+    this.http.post('https://jsonplaceholder.typicode.com/posts',body,{headers:header}).subscribe((data)=>{
+      console.log(data)
+      this.postJsonValue = data;
+    })
+  }
 }
